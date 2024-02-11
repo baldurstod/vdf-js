b40f4ef32f93adc04c512a150737bcbb8dbed2d7
Guillaume <Guillaume>
2024-02-11 18:11:00 +0100

    Added test for prettyPrint

--- src/test.js
+++ src/test.js
@@ -1,9 +1,10 @@
 import fs from 'fs/promises';
 import { parse } from './index.js';
+import { stringify } from './stringify.js';
 
-//parseVDF('./var/player_card_template.txt');
+parseVDF('./var/player_card_template.txt');
 //parseVDF('./var/items_game.txt');
-parseVDF('./var/mvm_popfile.pop');
+//parseVDF('./var/mvm_mannworks.pop');
 
 async function parseVDF(filePath) {
 	const contents = await fs.readFile(filePath/*, { encoding: 'utf8' }*/);
@@ -11,4 +12,5 @@ async function parseVDF(filePath) {
 
 	console.info(JSON.stringify(res));
 	console.info(res);
+	console.info(stringify(res, { prettyPrint: true }));
 }
